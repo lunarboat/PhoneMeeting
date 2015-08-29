13411fe00c2eb3e327bd65fab8ab14e67213b8ed
lunarboat <lunarboat@outlook.com>
2015-08-29 15:30:28 +0800

    Initial Commit

---
+++ PhoneMeeting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneMeeting
+//
+//  Created by lunarboat on 15/8/29.
+//  Copyright (c) 2015年 lunarboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
